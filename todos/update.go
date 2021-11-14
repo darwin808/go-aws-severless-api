811e5240079e073e5f6a9a74e06d0efcddb2aa00
darwin <darwin.apolinario@serino.com>
2021-11-14 14:41:45 +0800

    initialize server

---
+++ todos/update.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Item struct {
+	Id       string `json:"id,omitempty"`
+	UserName string `json:"userName"`
+	Message  string `json:"message"`
+	Picture  string `json:"picture"`
+}
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// Creating session for client
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	pathParamId := request.PathParameters["id"]
+
+	itemString := request.Body
+	itemStruct := Item{}
+	json.Unmarshal([]byte(itemString), &itemStruct)
+
+	fmt.Println("RARARARARAR", request)
+	info := Item{
+		UserName: itemStruct.UserName,
+		Message:  itemStruct.Message,
+		Picture:  itemStruct.Picture,
+	}
+
+	// Prepare input for Update Item
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":t": {
+				S: aws.String(info.UserName),
+			},
+			":d": {
+				S: aws.String(info.Message),
+			},
+			":p": {
+				S: aws.String(info.Picture),
+			},
+		},
+		TableName: aws.String(os.Getenv("DYNAMODB_TABLE")),
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(pathParamId),
+			},
+		},
+		ReturnValues:     aws.String("UPDATED_NEW"),
+		UpdateExpression: aws.String("set userName = :t, message = :d , picture = :p"),
+	}
+
+	// UpdateItem request
+	res, err := svc.UpdateItem(input)
+
+	fmt.Println("res", res)
+	// Checking for errors, return error
+	if err != nil {
+		fmt.Println(err.Error())
+		return events.APIGatewayProxyResponse{StatusCode: 500}, nil
+	}
+
+	// Marshal item to return
+	itemMarshalled, err := json.Marshal(info)
+	return events.APIGatewayProxyResponse{Body: string(itemMarshalled), StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
